86ed30ab194f1efcb22793601c94babb1f9caa1f
gust3065 <gustav2436@gmail.com>
2022-03-28 16:05:21 +0200

    tilføjet nogle ting til vekctortask

--- eksamensprojekt/Form_Vector_Task.cs
+++ eksamensprojekt/Form_Vector_Task.cs
@@ -12,7 +12,7 @@ namespace eksamensprojekt
 {
     public partial class Form_Vector_Task : Form
     {
-        double rightanswer = 0.0;
+        double rightanswer = 0.0; // erklærer en variable
         double rightanswerplus1 = 0.0;
         double rightanswerplus2 = 0.0;
         double rightanswerangle = 0.0;
@@ -30,7 +30,7 @@ namespace eksamensprojekt
             int num8 = random1.Next(0, 50);
             int num9 = random1.Next(0, 50);
             int num10 = random1.Next(0, 50);
-            rightanswer = Math.Round(Math.Sqrt((num1 * num1) + (num2 * num2)),1); // regner det rigtige svar.
+            rightanswer = Math.Round(Math.Sqrt((num1 * num1) + (num2 * num2)),1); // regner det rigtige svar og giver variablen et tal
             rightanswerplus1 = num3 + num5;
             rightanswerplus2 = num4 + num6;
             rightanswerangle = Math.Round(Math.Acos((num7*num9)+(num8*num10)/(Math.Round(Math.Sqrt((num7 * num7) + (num8 * num8)), 1)* Math.Round(Math.Sqrt((num9 * num9) + (num10 * num10)), 1))));
@@ -45,7 +45,6 @@ namespace eksamensprojekt
             this.Visible = false;
             f.Show();
             this.Close();
-            
         }
 
         private void button_Back_Click(object sender, EventArgs e)
@@ -57,39 +56,35 @@ namespace eksamensprojekt
 
         private void button_answer_Click(object sender, EventArgs e)
         {
-
             try
             {
-                if (double.Parse(textBox_answer3.Text) != rightanswerangle) // hvis svaret er forkert bliver knappen rød
+                if (double.Parse(textBox_answer.Text) != rightanswer) // hvis svaret er forkert bliver knappen rød
                 {
-                    textBox_answer3.BackColor = Color.Red;
+                    textBox_answer.BackColor = Color.Red;
                     label_answer.Text = "Forkert. Prøv igen";
-                    label_answer.Visible = true;
                 }
-                else if (double.Parse(textBox_answer3.Text) == rightanswerangle) // hvis svaret er rigtig bliver knappen grøn
+                else if (double.Parse(textBox_answer.Text) == rightanswer) // hvis svaret er rigtig bliver knappen grøn
                 {
-                    textBox_answer3.BackColor = Color.LightGreen;
+                    textBox_answer.BackColor = Color.LightGreen;
                     label_answer.Text = "Dit svaret er rigtig";
-                    label_answer.Visible = true;
                 }
             }
-            catch // fanger hvis man skriver et bogstav.
+            catch // fanger hvis man skriver et tal.
             {
                 Color c = textBox_answer.BackColor;
-                textBox_answer3.BackColor = Color.Red;
+                textBox_answer.BackColor = Color.Red;
                 MessageBox.Show("Error! you have to enter an number", "Format Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                textBox_answer3.BackColor = c;
-                textBox_answer3.Text = "";
-                textBox_answer3.Focus();
+                textBox_answer.BackColor = c;
+                textBox_answer.Text = "";
+                textBox_answer.Focus();
             }
         }
 
         private void button_answer2_Click(object sender, EventArgs e)
         {
-            //string[] answerarray = textBox_answer2.Text.Split(new char[] { ',' });
             try
             {
-                string[] answerarray = textBox_answer2.Text.Split(new char[] { ',' });
+                string[] answerarray = textBox_answer2.Text.Split(new char[] { ',' }); // så progammet genkender splittet er ved komma
                 if (!string.Equals(textBox_answer2.Text, rightanswerplus1.ToString() + "," + rightanswerplus2.ToString())) // hvis svaret er forkert bliver knappen rød
                 {
                     textBox_answer2.BackColor = Color.Red;
@@ -116,25 +111,25 @@ namespace eksamensprojekt
         {
             try
             {
-                if (double.Parse(textBox_answer.Text) != rightanswer) // hvis svaret er forkert bliver knappen rød
+                if (double.Parse(textBox_answer3.Text) != rightanswer) // hvis svaret er forkert bliver knappen rød
                 {
-                    textBox_answer.BackColor = Color.Red;
+                    textBox_answer3.BackColor = Color.Red;
                     label_answer.Text = "Forkert. Prøv igen";
                 }
                 else if (double.Parse(textBox_answer.Text) == rightanswer) // hvis svaret er rigtig bliver knappen grøn
                 {
-                    textBox_answer.BackColor = Color.LightGreen;
+                    textBox_answer3.BackColor = Color.LightGreen;
                     label_answer.Text = "Dit svaret er rigtig";
                 }
             }
             catch (Exception)
             {
-                Color c = textBox_answer2.BackColor;
-                textBox_answer2.BackColor = Color.Red;
+                Color c = textBox_answer3.BackColor;
+                textBox_answer3.BackColor = Color.Red;
                 MessageBox.Show("Error! you have to enter an number", "Format Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                textBox_answer2.BackColor = c;
-                textBox_answer2.Text = "";
-                textBox_answer2.Focus();
+                textBox_answer3.BackColor = c;
+                textBox_answer3.Text = "";
+                textBox_answer3.Focus();
             }
         }
     }
